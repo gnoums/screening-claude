7cad31a2de9d99e5ae5c3f9122c0c753e4306e66
gnoums <garcia.lara@live.com>
2026-02-23 19:19:38 -0800

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,8 +5,8 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}" style="display:block;padding-top:22px;padding-bottom:22px">
-                        <x-application-logo class="block fill-current text-gray-800" style="width:190px;height:auto" />
+                    <a href="{{ route('dashboard') }}" style="display:block;padding-top:26px;padding-bottom:22px">
+                        <x-application-logo class="block fill-current text-gray-800" style="width:185px;height:auto" />
                     </a>
                 </div>
 
